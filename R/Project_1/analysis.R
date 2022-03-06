d825416b16063c63b5f33ba9915fab0112d5ee77
LolipopnJoker <yoav.weller@gmail.com>
2022-03-06 12:53:48 +0200

    Adding: *An explanation about the data source file *An author attribute with my name *A source attribute with a link to the data source
    *Imports of tidyverse and the data

--- R/Project_1/analysis.R
+++ R/Project_1/analysis.R
@@ -1,1 +1,18 @@
 #' Analyzing the S&P stocks between the years 2012 to 2016.
+#' 
+#' Its name is fundamentals.csv. It includes metrics extracted from
+#' annual SEC 10K fillings (2012-2016), should be enough to derive most
+#' of popular fundamental indicators.
+#' 
+#' All the data in fundamentals are from Nasdaq Financials, extended by
+#' some fields from EDGAR SEC databases.
+#' 
+#' @author Yoav Weller
+#' @source https://www.kaggle.com/dgawlik/nyse/version/3
+
+# Imports ------------------------------------------
+
+library(tidyverse) # Importing tidyverse
+
+DB = read.csv('G:/My Drive/Portfolio/R/Project_1/fundamentals.csv') # Importing the data
+
