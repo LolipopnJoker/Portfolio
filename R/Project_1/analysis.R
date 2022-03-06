d393bafd4d0f16f3b5d467269d3639eb29bdaa78
LolipopnJoker <yoav.weller@gmail.com>
2022-03-06 20:41:16 +0200

    Changing the dataset and the formatting of the introduction.

--- R/Project_1/analysis.R
+++ R/Project_1/analysis.R
@@ -1,18 +1,16 @@
-#' Analyzing the S&P stocks between the years 2012 to 2016.
-#' 
-#' Its name is fundamentals.csv. It includes metrics extracted from
-#' annual SEC 10K fillings (2012-2016), should be enough to derive most
-#' of popular fundamental indicators.
-#' 
-#' All the data in fundamentals are from Nasdaq Financials, extended by
-#' some fields from EDGAR SEC databases.
-#' 
+#' @title Project 1 analysis
 #' @author Yoav Weller
-#' @source https://www.kaggle.com/dgawlik/nyse/version/3
+#' @source https://www.kaggle.com/sanjeetsinghnaik/top-1000-highest-grossing-movies
+#' @description Analyzing information about the top 1000 highest grossing holywood films. 
+#' It is up to date as of 10th January 2022.
+#' @details This data has been scraped from multiple site and has been added together
+#' for performing various datat operations. The data has been taken from idmb,
+#' rotten tomatoes and many other sites.
 
 # Imports ------------------------------------------
 
 library(tidyverse) # Importing tidyverse
 
-DB = read.csv('G:/My Drive/Portfolio/R/Project_1/fundamentals.csv') # Importing the data
+DB = read.csv('G:/My Drive/Portfolio/R/Project_1/dataset.csv', row.names = 1) # Importing the data
+
 
