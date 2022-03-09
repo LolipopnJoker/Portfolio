a49ce4d3168075815d7313985e00d3ce67f990bb
Yoav Weller <yoav.weller@gmail.com>
2022-03-09 11:45:42 +0200

    Update R analysis
    editing the comments

--- R/Project_1/analysis.R
+++ R/Project_1/analysis.R
@@ -1,17 +1,17 @@
 #' @title Project 1 analysis
 #' @author Yoav Weller
 #' @source https://www.kaggle.com/sanjeetsinghnaik/top-1000-highest-grossing-movies
-#' @description Analyzing information about the top 1000 highest grossing holywood films. 
+#' @description Analyzing information about the top 1000 highest grossing Hollywood films. 
 #' It is up to date as of 10th January 2022.
 #' @details This data has been scraped from multiple site and has been added together
-#' for performing various datat operations. The data has been taken from idmb,
+#' for performing various data operations. The data has been taken from IMDB,
 #' rotten tomatoes and many other sites.
 
 # Imports ------------------------------------------
 
 library(tidyverse) # Importing tidyverse
-library(tm)
-library(syuzhet)
+library(tm) # Importing tm
+library(syuzhet) # Importing syuzhet
 
 DB  <- read.csv('G:/My Drive/Portfolio/R/Project_1/dataset.csv', row.names = 1) # Importing the data.
                                                                                 # The first column is
@@ -22,8 +22,8 @@ DB  <- read.csv('G:/My Drive/Portfolio/R/Project_1/dataset.csv', row.names = 1) 
 # Data Cleaning ------------------------------------
 
 # Splitting the date column
-DB <- separate(DB, Release.Date, c("release.month", "release.day", "release.year"), sep = " ")
-DB <- separate(DB, Genre, c("Genre.1", "Genre.2", "Genre.3", "Genre.4", "Genre.5", "Genre.6"), sep = ", ")
+DB <- separate(DB, Release.Date, c("release.month", "release.day", "release.year"), sep = " ") # Separating the date column
+DB <- separate(DB, Genre, c("Genre.1", "Genre.2", "Genre.3", "Genre.4", "Genre.5", "Genre.6"), sep = ", ")# Separating the genre column
 
 # Cleaning from unwanted symbols that might affect the analysis.
 DB[] <- lapply(DB, gsub, pattern=',', replacement='') # Deleting all the , symbols.
