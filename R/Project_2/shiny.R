952e58967af033a9a128e72ddcccdfeba2d5bbe1
Yoav Weller <yoav.weller@gmail.com>
2022-03-26 10:35:37 +0300

    Update shiny.R
    Adding an UI element and a sever element

--- R/Project_2/shiny.R
+++ R/Project_2/shiny.R
@@ -1,3 +1,20 @@
 # Imports
 library(shiny)
 library(shinythemes)
+
+ui <- fluidPage(
+  title = 'Follow The Money',
+  sidebarLayout(
+    sidebarPanel(
+      textInput('txtInput', 'Input the text to display')
+    ),
+    mainPanel(
+      paste('You typed'),
+      textOutput('txtOutput')
+    )
+  )
+)
+
+server <- function(input, output){}
+
+shinyApp(ui=ui, server = server)
