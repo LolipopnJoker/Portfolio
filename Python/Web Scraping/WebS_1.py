1d2cad82eaee1306d4a657b08863ddebee368d1c
LolipopnJoker <yoav.weller@gmail.com>
2022-03-29 13:09:15 +0300

    Create WebS_1.py
    Creating webS_1 file. This fie already contains few classes (it was written by me few months ago).

---
+++ Python/Web Scraping/WebS_1.py
@@ -0,0 +1,57 @@
+"""In this file, I created number of classes."""
+
+__author__ = "Yoav Weller"
+__copyright__ = "Copyright 2022, Purple Dove"
+__credits__ = ["Yoav Weller"]
+__license__ = "GPL"
+__version__ = "1.0.0"
+__maintainer__ = "Yoav Weller"
+__email__ = "yoav.weller@gmail.com"
+__status__ = "Production"
+
+class data_from_news(object):
+    """
+    This is the main class. It defines the basic features that needs to be establish
+    for each news source.
+    """
+    def __init__(self, url):
+        """ Importing """
+        import requests
+        from bs4 import BeautifulSoup
+        import pandas as pd
+
+        self.url = url
+        
+        self.page = requests.get(self.url)
+        self.soup = BeautifulSoup(self.page.text, 'html.parser')
+
+        """
+        Creating an empty dataframe for the raw data:
+        """
+        # This is a dictionary with the name of the headers of the dataframe + one empty observation.
+        basic_headers = {
+            'SOURCE': ['NA'], # The name of the site (e.g. 'Forbs')
+            'Link': ['NA'], # The link for the article
+            'Title': ['NA'], # Main heading of the article
+            'Subtitle': ['NA'], # Subheading of the article
+            'Body': ['NA'] # The rest of the articls conent
+            }
+        
+        news_raw_data = pd.DataFrame(basic_headers) # Creating the table.
+        
+        return news_raw_data
+    
+class data_from_forbs(data_from_news):
+    
+    def __init__(self, url):
+        
+        super().__init__(url)
+    
+    def search_data(self):
+        titles = []
+        for title in self.soup.find_all('a'):
+            titles.append(title.text)
+
+forbs_link = str("https://www.forbes.com/money/?sh=65fbdbb1c19a")
+yahoo_link = 'https://finance.yahoo.com/'
+
