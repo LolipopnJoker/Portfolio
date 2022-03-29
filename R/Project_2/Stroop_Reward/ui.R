412b33ba8875b6f139714785c57a87f2edd87b47
LolipopnJoker <yoav.weller@gmail.com>
2022-03-29 12:45:55 +0300

    Update ui.R
    Deleting unnecessary lines of code.

--- R/Project_2/Stroop_Reward/ui.R
+++ R/Project_2/Stroop_Reward/ui.R
@@ -10,20 +10,6 @@
 library(shiny)
 library(glue)
 
-
-
-green_rgb <- c(0, 255, 0)
-blue_rgb <- c(0, 0, 255)
-yellow_rgb <- c(234, 221, 202)
-pink_rgb <- c(255,192,203)
-purple_rgb <- c(153,50,204)
-color_codes <- list(green_rgb,
-                    blue_rgb,
-                    yellow_rgb,
-                    pink_rgb,
-                    purple_rgb)
-
-displayed_color <- sample(color_codes, 1)
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
 
