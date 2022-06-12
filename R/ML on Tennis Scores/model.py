31bcb39f8a8144138a511970791360eb9339062e
LolipopnJoker <yoav.weller@gmail.com>
2022-06-12 16:04:28 +0300

    Update model.py
    Creating documentation and importing pandas

---
+++ R/ML on Tennis Scores/model.py
@@ -0,0 +1,13 @@
+"""In this file, I created number of classes."""
+
+__author__ = "Yoav Weller"
+__copyright__ = "Copyright 2022, Yoav Weller"
+__credits__ = ["Yoav Weller"]
+__license__ = "GPL"
+__version__ = "1.0.0"
+__maintainer__ = "Yoav Weller"
+__email__ = "yoav.weller@gmail.com"
+__status__ = "Production"
+
+# Modules Imports
+import pandas as pd
